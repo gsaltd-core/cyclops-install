7b564a18ed5f79687b9d743cc58da7e13e465cc0
gsaltd-core <55751664+gsaltd-core@users.noreply.github.com>
2020-11-24 14:10:01 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl -H 'Authorization: token <INSERT TOKEN>' -H 'Accept: application/vnd.github.v3.raw' -L https://github.com/gsaltd-core/cyclops-docker-logging/archive/main.zip -o cyclops-docker-logging.zip 
+unzip cyclops-docker-logging 
+rm cyclops-docker-logging.zip 
