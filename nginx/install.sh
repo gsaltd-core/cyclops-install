bd00b6b2c86655a9bd5323059e753783f1ad102f
Andy Brough <andy.brough@gsaltd.com>
2020-11-30 14:02:39 +0000

    nginx install

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,7 +1,10 @@
 GIT_TOKEN=<INSERT TOKEN>
-GIT_BRANCH=sandbox
-curl -H 'Authorization: token $GIT_TOKEN' -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/nginx.conf -o cyclops-docker/nginx.conf 
-#curl -H 'Authorization: token $GIT_TOKEN' -H 'Accept: application/vnd.github.v3.raw' -L https://github.com/gsaltd-core/cyclops-docker/archive/main.zip -o cyclops-docker.zip 
+GIT_BRANCH=main
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/nginx.conf -o cyclops-docker-$GIT_BRANCH/nginx.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-dynamic.conf -o cyclops-docker/$GIT_BRANCH.conf.d/shared-dynamic.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-static.conf -o cyclops-docker/$GIT_BRANCH.conf.d/shared-static.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/apollo.conf -o cyclops-docker/$GIT_BRANCH.conf.d/apollo.conf 
+
 #unzip -o cyclops-docker 
 #mv cyclops-docker-main cyclops-docker 
 #rm cyclops-docker.zip 
