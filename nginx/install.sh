a59decd0ace5598db150180fabcf0bcf38501ffc
Andy Brough <andy.brough@gsaltd.com>
2020-11-30 11:28:05 +0000

    nginx install

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,4 +1,7 @@
-curl -H 'Authorization: token <INSERT TOKEN>' -H 'Accept: application/vnd.github.v3.raw' -L https://github.com/gsaltd-core/cyclops-docker/archive/main.zip -o cyclops-docker.zip 
-unzip -o cyclops-docker 
-mv cyclops-docker-main cyclops-docker 
-rm cyclops-docker.zip 
+GIT_TOKEN=<INSERT TOKEN>
+GIT_BRANCH=sandbox
+curl -H 'Authorization: token $GIT_TOKEN' -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/nginx.conf -o cyclops-docker/nginx.conf 
+#curl -H 'Authorization: token $GIT_TOKEN' -H 'Accept: application/vnd.github.v3.raw' -L https://github.com/gsaltd-core/cyclops-docker/archive/main.zip -o cyclops-docker.zip 
+#unzip -o cyclops-docker 
+#mv cyclops-docker-main cyclops-docker 
+#rm cyclops-docker.zip 
