1d77a6d64d8cb528f03f908250020b33c03c3af5
Andy Brough <andy.brough@gsaltd.com>
2020-11-30 17:09:14 +0000

    nginx install

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,8 +1,9 @@
 GIT_TOKEN=
 GIT_BRANCH=main
-mkdir cylops-docker
+mkdir cyclops-docker
 mkdir cyclops-docker/conf.d
 mkdir cyclops-docker/certs
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/docker-compose.yml -o cyclops-docker/docker-compose.yml 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/nginx.conf -o cyclops-docker/nginx.conf 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-dynamic.conf -o cyclops-docker/conf.d/shared-dynamic.conf 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-static.conf -o cyclops-docker/conf.d/shared-static.conf 
