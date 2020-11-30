7cc44de9e77a9c44c8865bd0b04b81912ba7d72b
Andy Brough <andy.brough@gsaltd.com>
2020-11-30 15:39:47 +0000

    nginx install

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,9 +1,9 @@
 GIT_TOKEN=
 GIT_BRANCH=main
-curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker-$GIT_BRANCH/nginx.conf -o cyclops-docker-$GIT_BRANCH/nginx.conf 
-curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker-$GIT_BRANCH/conf.d/shared-dynamic.conf -o cyclops-docker/$GIT_BRANCH/conf.d/shared-dynamic.conf 
-curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker-$GIT_BRANCH/conf.d/shared-static.conf -o cyclops-docker/$GIT_BRANCH/conf.d/shared-static.conf 
-curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker-$GIT_BRANCH/conf.d/apollo.conf -o cyclops-docker/$GIT_BRANCH.conf.d/apollo.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/nginx.conf -o cyclops-docker-$GIT_BRANCH/nginx.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-dynamic.conf -o cyclops-docker-$GIT_BRANCH/conf.d/shared-dynamic.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-static.conf -o cyclops-docker-$GIT_BRANCH/conf.d/shared-static.conf 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/apollo.conf -o cyclops-docker-$GIT_BRANCH.conf.d/apollo.conf 
 
 #unzip -o cyclops-docker 
 #mv cyclops-docker-main cyclops-docker 
