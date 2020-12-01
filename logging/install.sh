f6ae839bdb8d6320a34391adc4625e87b0d13f1f
Andy Brough <andy.brough@gsaltd.com>
2020-12-01 17:49:53 +0000

    nginx install

--- logging/install.sh
+++ logging/install.sh
@@ -1,8 +1,8 @@
 GIT_TOKEN=
-GIT_BRANCH=sandbox
+GIT_BRANCH=main
 mkdir cyclops-docker-logging
-mkdir cyclops-docker-logging\logstash
-mkdir cyclops-docker-logging\logstash\pipeline
+mkdir cyclops-docker-logging/logstash
+mkdir cyclops-docker-logging/logstash/pipeline
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker-logging/$GIT_BRANCH/docker-compose.yml -o cyclops-docker-logging/docker-compose.yml 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker-logging/$GIT_BRANCH/logstash/pipeline/logstash.conf -o cyclops-docker-logging/logstash/pipeline/logstash.conf 
 
