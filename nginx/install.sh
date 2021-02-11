090b5651464762fa9d7b45e32576f09266d76a89
Peter Whiting <peter.whiting@corp.gsaltd.com>
2021-02-11 17:22:26 +0000

    Corrected default.conf copy error and removed mkdir common command

--- nginx/install.sh
+++ nginx/install.sh
@@ -4,13 +4,12 @@ GIT_BRANCH=main
 
 mkdir cyclops-docker
 mkdir cyclops-docker/conf.d
-mkdir cyclops-docker/conf.d/shared
 mkdir cyclops-docker/certs
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/docker-compose.yml -o cyclops-docker/docker-compose.yml 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/nginx.conf -o cyclops-docker/nginx.conf 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-dynamic.conf -o cyclops-docker/conf.d/shared-dynamic.conf 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-static.conf -o cyclops-docker/conf.d/shared-static.conf 
-curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/shared-static.conf -o cyclops-docker/conf.d/default.conf
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/conf.d/default.conf -o cyclops-docker/conf.d/default.conf
 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/conf.d/apollo.conf -o cyclops-docker/conf.d/$ENV-apollo.conf;
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/conf.d/athena.conf -o cyclops-docker/conf.d/$ENV-athena.conf; 
