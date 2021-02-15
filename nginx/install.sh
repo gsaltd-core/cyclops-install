efe52b8f841a718507c7292b4a9d557d9b164e6b
Peter Whiting <peter.whiting@corp.gsaltd.com>
2021-02-15 12:25:05 +0000

    added curl for restws.conf

--- nginx/install.sh
+++ nginx/install.sh
@@ -18,6 +18,7 @@ curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/conf.d/docviewer.conf -o cyclops-docker/conf.d/$ENV-docviewer.conf; 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/conf.d/pluto.conf -o cyclops-docker/conf.d/$ENV-pluto.conf; 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/conf.d/jupiter.conf -o cyclops-docker/conf.d/$ENV-jupiter.conf; 
+curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/conf.d/restws.conf -o cyclops-docker/conf.d/$ENV-restws.conf; 
 
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/certs/cyclops.crt -o cyclops-docker/certs/cyclops.crt
 curl -H "Authorization: token $GIT_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://raw.githubusercontent.com/gsaltd-core/cyclops-docker/$GIT_BRANCH/$ENV/certs/cyclops.key -o cyclops-docker/certs/cyclops.key
